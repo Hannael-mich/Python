21094742f944d59d9137209aeed972f3485e56a2
Hannael-mich <thanna.lkuz@gmail.com>
2023-03-11 02:54:20 -0600

    Solo se puso una y

--- HolaMundo.py
+++ HolaMundo.py
@@ -8,4 +8,4 @@ print("Peso total de 54 muñecas:" ,muneca*54 )
 sumapesos= (payasos*23) + (muneca*54)
 print("Peso total de los 23 payasos y las 54 muñecas: ",sumapesos )
 #Fin del ejercico2
-#Solo esto agregando un comentario
+#Solo estoy agregando un comentario
