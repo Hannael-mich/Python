6db949de787c821a33ea37b7fcd2cc3e9f95a6f2
Hannael-mich <thanna.lkuz@gmail.com>
2023-03-11 02:18:19 -0600

    Primer commit en python

---
+++ HolaMundo.py
@@ -0,0 +1,11 @@
+print (((3+2)/(2*5))**2) #Ejercicio1
+
+#Comiento del ejercicio2
+payaso = 112
+muneca = 75
+print("Peso total de 23 payasos:" ,payaso*23 )
+print("Peso total de 54 muñecas:" ,muneca*54 )
+sumapesos= (payaso*23) + (muneca*54)
+print("Peso total de los 23 payasos y las 54 muñecas: ",sumapesos )
+#Fin del ejercico2
+
