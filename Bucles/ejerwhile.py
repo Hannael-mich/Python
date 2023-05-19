dff839700e2665dd86ba3d960e097a4fd3626a69
Hannael-mich <thanna.lkuz@gmail.com>
2023-05-18 19:55:00 -0600

    Se agregaron dos ejercicios while

---
+++ Bucles/ejerwhile.py
@@ -0,0 +1,7 @@
+edad = int(input("Introduzca su edad "))
+i=0
+print("los años que han transcurridos son: ")
+
+while i < edad:
+    i+=1
+    print(i)
---
+++ Bucles/while.py
@@ -0,0 +1,8 @@
+num = int(input("Introduzca un numero "))
+
+print ("La tabla de multiplicar de ese numero es:")
+i = 0
+while i < 10:
+    i+=1
+    print(num*i)
+    
