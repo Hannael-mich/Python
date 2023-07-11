e38a6082590dbd085f171ef5cc987fc661a85b71
Hannael-mich <thanna.lkuz@gmail.com>
2023-07-10 23:25:35 -0600

    Se modifico un comentario en el codigo

--- POO/Ejercicio5.py
+++ POO/Ejercicio5.py
@@ -48,8 +48,8 @@ estudiante.agregar_carrera(carrera)
 # Imprimir la información del estudiante
 estudiante.imprimir_informacion()
 
+#Forma de como se hizo en Udemy
 
-#Crear un programa con tres clases Universidad, con atributos nombre (Donde se almacena el nombre de la Universidad). Otra llamada Carerra, con los atributos especialidad (En donde me guarda la especialidad de un estudiante). Una ultima llamada Estudiante, que tenga como atributos su nombre y edad. El programa debe imprimir la especialidad, edad, nombre y universidad de dicho estudiante con un objeto llamado persona.
 class Universidad():
     def __init__(self,Nombre):
         self.Nombre = Nombre
